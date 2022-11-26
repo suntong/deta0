4f6ee67961947bfae60086b29932a81444beaae3
Tong Sun <suntong@cpan.org>
2022-11-26 12:10:11 +0800

    - [+] add & use fs

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const fs = require('fs')
+
 const express = require('express');
 
 const app = express(); 
-
 app.get('/', async (req, res) => {
+  fs.writeFileSync('/tmp/abc','def')
   res.send('Hello World')
 });
 
