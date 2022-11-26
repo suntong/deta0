5746aeea0aea0286600eb7277e18180870f006db
suntong <suntong@cpan.org>
2022-11-25 23:11:44 -0500

    - [+] call fs.renameSync

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express(); 
 app.get('/', async (req, res) => {
   fs.writeFileSync('/tmp/abc','def')
+  fs.renameSync('/tmp/abc','/tmp/def')
   res.send('Hello World')
 });
 
